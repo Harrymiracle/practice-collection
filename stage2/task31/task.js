857637ae9785641d71b2d3fbbfc1839216962e14
Harrymircle <2018436718@qq.com>
2020-05-22 14:12:47 +0800

    tab切换

--- stage2/task31/task.js
+++ stage2/task31/task.js
@@ -1,17 +1,17 @@
-let data = [
+let resData = [
     {
         'city': '上海',
-        'cityId': 0001,
+        'cityId': '0001',
         'schools': ['复旦大学', '上海交通大学', '同济大学']
     },
     {
         'city': '北京',
-        'cityId': 0002,
+        'cityId': '0002',
         'schools': ['北京大学', '清华大学', '北京航空航天大学']
     },
     {
         'city': '杭州',
-        'cityId': 0007,
+        'cityId': '0007',
         'schools': ['浙江大学', '杭州电子科技大学', '浙江工业大学']
     }
 ];
@@ -25,28 +25,47 @@ window.onload = function () {
         school = qs('.school');
 
     listenTab();
+    render(deal(resData, resData[0]['cityId'], true));
 
+    // 注册事件监听
     function listenTab() {
         addEvent(tab, 'change', function () {
             if (tab_item[0].firstElementChild.checked) {
                 insch.className = 'insch';
                 outsch.className = 'outsch hidden';
+                render(deal(resData, resData[0]['cityId'], true));
             } else if (tab_item[1].firstElementChild.checked) {
                 insch.className = 'insch hidden';
                 outsch.className = 'outsch';
             }
-            render();
+        })
+
+        addEvent(city, 'change', function () {
+            // console.log(city.selectedIndex);  // selectedIndex当前option的选中项的index
+            render(deal(resData, this.value, false));
         })
     }
 
-    function setData(index = 0) {
-
+    // 处理数据，返回处理后的DOM字符串  初始化时才处理城市下的option
+    function deal(arr, checkedId, isInit) {
+        let tempCity = '', tempSchool = '', Selected = '';
+        arr.forEach(item => {
+            checkedId === item.cityId ? Selected = 'selected' : Selected = '';
+            isInit && (tempCity += '<option value="' + item.cityId + '"' + Selected + '>' + item.city + '</option>');
+            checkedId === item.cityId && item.schools.map(n => {
+                tempSchool += '<option value="' + n + '">' + n + '</option>';
+            })
+        });
+        return {
+            isInit,
+            tempCity,
+            tempSchool
+        }
     }
 
-    function render(arr) {
-        let temp = '';
-        arr.forEach(item=>{
-            temp += '<option value="'+item+'">'
-        })
+    // 渲染字符串到对应元素
+    function render(obj) {
+        if (obj.isInit) { city.innerHTML = obj.tempCity; }
+        school.innerHTML = obj.tempSchool;
     }
 }
